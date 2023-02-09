15f94882ce584f1297ab9f0ea7a8a8b092d42fda
Alex <a.murogov@gmail.com>
2023-02-09 20:15:13 +0000

    create model

---
+++ models/staging/stg_green_tripdata.sql
@@ -0,0 +1,7 @@
+{{ config(materialized="view") }}
+
+select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+from {{ source("staging", "green_tripdata") }}
+where vendorid is not null
+limit 10 
+
