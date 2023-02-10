4ab3170d434aab29670adcd5c2557baad3c1b3ef
Alex <a.murogov@gmail.com>
2023-02-10 20:32:26 +0000

    first views created

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -1,6 +1,6 @@
 {{ config(materialized="view") }}
 
-select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+select *--, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
 from {{ source("staging", "green_tripdata") }}
 where vendorid is not null
 limit 10 
--- models/staging/stg_yellow_tripdata.sql
+++ models/staging/stg_yellow_tripdata.sql
@@ -1,1 +1,8 @@
 {{ config(materialized='view') }}
+
+
+  select *
+    --row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_tripdata') }}
+  where vendorid is not null 
+  limit 10 
